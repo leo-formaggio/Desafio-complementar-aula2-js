3fb8c01cbc8afd93328b65ea084a7f7996e919b4
Leonardo Formaggio <forleo1@hotmail.com>
2024-10-17 15:14:28 -0300

    Desafio complementar Coder

---
+++ script.js
@@ -0,0 +1,15 @@
+// Exercícios do desafio complementar
+ var nomeInserido = prompt("Digite seu nome: ");
+ alert("Bem vindo, " + nomeInserido);
+
+var numUm = parseInt(prompt("Digite um número: "));
+var numDois = parseInt(prompt("Digite outro número: "));
+
+var resultado = numUm * numDois;
+
+alert("O resultado é: " + resultado);
+
+var textoUm = prompt("Digite uma palavra: ");
+var textoDois = prompt("Digite outra palavra: ");
+
+alert("Reposta: " + textoUm + " " + textoDois);
